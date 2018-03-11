75e5d949a6fb4060f3e1007624538a6b12f22f79
danielhhowell <danielhhowell@aol.com>
2018-03-11 18:26:11 -0500

    ~

--- Flasker.py
+++ Flasker.py
@@ -5,8 +5,6 @@ import Main
 # App config.
 DEBUG = True
 app = Flask(__name__)
-#app.config.from_object(__name__)
-#app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 
 class InputForm(Form):
