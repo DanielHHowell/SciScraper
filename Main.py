caa0a27f386c04b219f3b7bc2f224726f5ae3842
Daniel <danielhhowell@aol.com>
2018-04-11 01:57:29 -0500

    -

--- Main.py
+++ Main.py
@@ -5,19 +5,20 @@ import KeywordAnalysis
 from collections import OrderedDict
 
 
-def sciscraper(topic,queries,nResults,sortby):
-    PMCIDs, query = [PMC for PMC in PageScraper.esearch(topic, queries, nResults, sortby)]
-    mainDict = OrderedDict((PMC, PageScraper.esummary(PMC)) for PMC in PMCIDs)
-    for PMC in mainDict:
-        mainDict[PMC]['Images'] = PageScraper.image_scraper(PMC)
-    alltext = [KeywordAnalysis.text_grab(i) for i in PMCIDs]
+def sciscraper(topic, queries, nResults, sortby):
+    pmc_ids, query = [pmc for pmc in PageScraper.esearch(topic, queries, nResults, sortby)]
+    main_dict = OrderedDict((pmc, PageScraper.esummary(pmc)) for pmc in pmc_ids)
+    for pmc in main_dict:
+        main_dict[pmc]['Images'] = PageScraper.image_scraper(pmc)
+    alltext = [KeywordAnalysis.text_grab(i) for i in pmc_ids]
     keywords = [i for i in KeywordAnalysis.get_continuous_chunks(" ".join(alltext)) if i != topic]
-    return mainDict, query, keywords
+    return main_dict, query, keywords
 
-def sql_insert(mainDict, query):
+
+def sql_insert(main_dict, query):
     sqlbackend.create_table()
-    for i in mainDict:
-        ref = mainDict[i]
+    for i in main_dict:
+        ref = main_dict[i]
         sqlbackend.insert(query, i, ref['DOI'], ref['Title'], ', '.join(ref['Authors']),
                           ref['Date'], ref['Abstract'], ', '.join(ref['Images']))
     return
